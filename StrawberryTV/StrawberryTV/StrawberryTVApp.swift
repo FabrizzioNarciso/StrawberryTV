a77ac139c145978b326818474e91ecba99b2da00
FabrizzioNarciso <134290687+FabrizzioNarciso@users.noreply.github.com>
2023-05-31 10:32:39 -0300

    Add files via upload

---
+++ StrawberryTV/StrawberryTV/StrawberryTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrawberryTVApp.swift
+//  StrawberryTV
+//
+//  Created by Fabrizio Narciso on 31/05/23.
+//
+
+import SwiftUI
+
+@main
+struct StrawberryTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
