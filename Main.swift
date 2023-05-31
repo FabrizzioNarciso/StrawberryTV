184b8730dc6acf97691f2e921dc371561aa976ad
Anderson Vulto <64989532+andersonvulto@users.noreply.github.com>
2023-05-31 10:26:03 -0300

    Add files via upload
    Adicionando o Main file e o banco de dados

---
+++ Main.swift
@@ -0,0 +1,33 @@
+//
+//  PeachTVApp.swift
+//  PeachTV
+//
+//  Created by Anderson  Vulto on 29/05/23.
+//
+
+import SwiftUI
+
+let images = Images()
+
+@main
+struct PeachTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            TabView {
+                NavigationView{
+                    Buscar()
+                }
+                    .tabItem {
+                        Label("Buscar", systemImage: "magnifyingglass")
+                    }
+                NavigationView {
+                    SomeView()
+                }
+                    .tabItem {
+                        Label("category", systemImage: "chevron.right")
+                    }
+            }.environmentObject(images)
+                .preferredColorScheme(.dark)
+        }
+    }
+}
